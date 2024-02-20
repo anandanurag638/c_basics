b7f14b9dcfe6f08dfebb03376125cea3cdd3b3b9
Your Name <your.email@example.com>
2024-02-20 07:33:23 +0530

    some minimal changes

---
+++ exercise1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    // int i,  num;
+    // printf("give me your number you want to multiply ");
+    // scanf("%d" ,&num);
+    // printf("your given number is %d", num);
+    // for ( i = 1; i < 11; i++)
+    // {
+    //     printf("%d * %d = %d\n", num, i , num * i);
+    // }
+    
+    return 0;
+}
