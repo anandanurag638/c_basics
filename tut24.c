9c247b7a0319fb6e5bc0a94ebe53037153777736
Your Name <your.email@example.com>
2024-02-26 21:33:16 +0530

    small changes

--- tut24.c
+++ tut24.c
@@ -3,7 +3,6 @@ int main(){
     int km, m, pound, kg, converse;
     printf("give your converstion \n");
     scanf("%d", &converse);
-    printf("you gave this to convert %c" , converse);
-    
-    
-} 
+    printf("you gave this to convert %d" , converse);
+    printf("distance in meters will be %f", (km) * (100.00));
+}
