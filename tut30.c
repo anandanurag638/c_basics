b6ba34c8987d0539f0608142c6f50ee43920b45a
Your Name <your.email@example.com>
2024-02-28 21:21:31 +0530

    minor changes

--- tut30.c
+++ tut30.c
@@ -1,18 +1,51 @@
-#include<stdio.h>
-int main(){
-    int choice, sizeoftriangle;
-    printf("choose 1 for triangular pattern and 0 for reversed triangular pattern");
-    scanf("%d" , &choice);
-    printf("how big is your star triangle");
-    scanf("%d", &sizeoftriangle);
-    if (choice == 0)
-    
+#include <stdio.h>
+int main()
+{
+    int choice, rows, i, j;
+    printf("choose 1 for triangular pattern \nchoose 0 for reversed triangular pattern\n");
+    scanf("%d", &choice);
+    printf("how many rows in  your star triangle\n");
+    scanf("%d", &rows);
+    if (choice == 1)
+
     {
-        /* code */ for (int  i = 0; i < sizeoftriangle; i++)
+        printf("triangular star pattern  \n");
+        /* code */ for (int i = 1; i <= rows; i++)
         {
-            /* code */printf(" * ");
+            for (j = 1; j <= i; j++)
+            {
+                /* code */ printf("*");
+                
+            }
+            printf("\n");
         }
         
+            
+        
+
+        /* code */
     }
-    
+
+     else if (choice == 0)
+     {
+        /* code */ printf("reverse triangular pattern \n");
+
+        for (i = rows; i >= 1; i--)
+        {
+            for (j = 1; j <= i; j++)
+            {
+                /* code */ printf("*");
+                
+            }
+            printf("\n");
+        }
+
+        /* code */
+    }
+    else
+    {
+        printf("invalid pattern");
+        
+    }
+    return 0;
 }
