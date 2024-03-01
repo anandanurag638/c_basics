af95be9791a7a0e9996ff6311149ec88a9f91cf3
Your Name <your.email@example.com>
2024-03-01 18:59:02 +0530

    tutorial codes

--- tut31.c
+++ tut31.c
@@ -15,3 +15,32 @@ int main()
     printf("%d , %d" , a, b);
     return 0 ;
 }
+
+
+
+
+
+#include<stdio.h>
+void numberprinter(int *x, int  *y)
+{
+    int sum, substraction;
+  sum = *x + *y;
+printf("here is the addition of the numbers \n %d\n" , sum );
+
+substraction = *x - *y;
+printf("here is the substraction\n %d\n", substraction);
+printf("%d , %d" , *x, *y);
+
+}
+
+
+
+
+int main(){
+    int a,b;
+    printf("give your first number");
+    scanf("%d" , &a);
+    printf("give your second number");
+   scanf("%d" , &b);
+   numberprinter(&a,&b);
+}
