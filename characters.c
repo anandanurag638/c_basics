1af8d79c4a1194c1105b64633a7b5201154dd9b3
Your Name <your.email@example.com>
2024-03-03 12:06:03 +0530

    changes for string printing

--- characters.c
+++ characters.c
@@ -2,7 +2,24 @@
 int main(){
      char alpha[4];
      gets(alpha);
-     printf(" your word is  %s\n",alpha); 
+     printf(" your word is  %s\n",alpha);
      return 0;
-    
+
+}
+
+#include <stdio.h>
+int main()
+{
+    char name[] = {'a',
+                   'n',
+                   'u',
+                   'r',
+                   'a',
+                   'g',
+                   '\0'};
+
+printf("your name is %s", name);
+return 0;
+
+
 }
