2bb6cfdcd3993a5a8dc1456cf369c2077afa3d78
Your Name <your.email@example.com>
2024-03-12 18:39:16 +0530

    new string functions

---
+++ string35.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+// int main()
+// {
+//     char nme[] = {'a', 'n', 'u', '\0'};
+//     printf("%s", nme);
+//     return 0;
+// }
+#include<stdio.h>
+#include<string.h>
+int main(){
+char n1 [] = "anurag";
+char n2 [] = "anand";
+puts(strcat(n1, n2));
+}
