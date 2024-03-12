79cdc0a77d7053ab6f3703f4e212fc799a38a137
Your Name <your.email@example.com>
2024-03-12 17:50:04 +0530

    some changes

--- tut31.c
+++ tut31.c
@@ -7,14 +7,14 @@ void swap(int *x, int *y) {
     return;
 }
 
-int main()
-{
-    int a = 21 , b =34 ;
-    printf("%d , %d\n",  a ,b);
-    swap(&a, & b);
-    printf("%d , %d" , a, b);
-    return 0 ;
-}
+// int main()
+// {
+//     int a = 21 , b =34 ;
+//     printf("%d , %d\n",  a ,b);
+//     swap(&a, & b);
+//     printf("%d , %d" , a, b);
+//     return 0 ;
+// }
 
 
 
