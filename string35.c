0786161c6f2b10d1c1cdcd33113e1e8c07be10bc
Your Name <your.email@example.com>
2024-03-13 06:35:39 +0530

    modification

--- string35.c
+++ string35.c
@@ -10,5 +10,14 @@
 int main(){
 char n1 [] = "anurag";
 char n2 [] = "anand";
-puts(strcat(n1, n2));
+char n3 [151];
+printf("here is concatinated string value\n %s\n",strcat(n1, n2));
+printf("here is length of your string\n  %d \n",strlen(n2));
+printf("here is the reverse of the word \n %d\n", strerror );
+strcpy(n3 , n1);
+printf("%s /n", n3);
+strcpy(n3 , n2);
+printf("  %s", n3);
+
+return 0;
 }
