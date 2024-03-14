2ed8bdf69f6d5ac82b3888c93ff57bcf7c360bc0
Your Name <your.email@example.com>
2024-03-14 14:33:30 +0530

    minor changes

---
+++ structure.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+struct info
+{
+    /* data */int age;
+    char fc;
+    float marks;
+}; 
+int main(){
+
+
+struct structure
+{
+    /* data */
+};
+ info anurag, ankit, saurav;
+}
